e4cfbd48d276bfac8606563680215687cef677dc
Christoph Rudolf <christoph.rudolf@tum.de>
2017-11-18 11:32:43 +0100

    Finished extraction image.

--- scripts/extract_logo.py
+++ scripts/extract_logo.py
@@ -12,12 +12,12 @@ def filterLogo(img, x, y, w, h):
     imgArray = np.asarray(img)[y-1:y+h+1, x-1:x+w+1]
     array3h = imageSource.apply3filter(imageSource.colorToBw(imgArray), horizontal_filter)
     array3v = imageSource.apply3filter(imageSource.colorToBw(imgArray), vertical_filter)
-    return Image.fromarray(np.uint8(imageSource.threshold(imageSource.maxBw(imageSource.pythagoras(array3h,array3v),255), 30)))
+    return Image.fromarray(np.uint8(imageSource.threshold(imageSource.maxBw(imageSource.pythagoras(array3h,array3v),255), 30))[1:h,1:w])
 
 if len(sys.argv) < 3:
     print("Insufficient parameter given. Expects [extract_logo] train_data_path logo_repo_path")
     sys.exit(1)
-else:   
+else:
     trainPath = sys.argv[1]
     repoPath = sys.argv[2]
 
@@ -27,32 +27,44 @@ positions = set()
 
 # walk over all stations
 for station in os.listdir(trainPath):
-    path = os.path.join(trainPath, station)
+    stationPath = os.path.join(trainPath, station)
     # walk over all logo types
-    for logo in os.listdir(path):
+    for logo in os.listdir(stationPath):
         # see if there is a meta data file or no logo is expected
-        path = os.path.join(path, logo)
-        metadata_file_path = os.path.join(path, METADATA_FILE)
+        logoPath = os.path.join(stationPath, logo)
+        metadata_file_path = os.path.join(logoPath, METADATA_FILE)
+        print(metadata_file_path)
+
         if os.path.exists(metadata_file_path):
             # read the metadata
             with open(metadata_file_path) as metadata:
                 reader = csv.reader(metadata, delimiter=',')
-                line = next(reader)
+                line = list(reader)[len(list(reader))-1]
                 c = line[0]
                 x, y, w, h = [int(a) for a in line[1:]]
                 
                 positions.add((x,y,w,h))
                 
                 # walk over all samples
-                for sample in os.listdir(path):
+                for i, sample in enumerate(os.listdir(logoPath)):
                     if sample.endswith(".jpg"):
-                        imagePath = os.path.join(path, sample)
+                        imagePath = os.path.join(logoPath, sample)
                         print("Working on file {}".format(imagePath))
 
                         sampleImage = Image.open(imagePath)
 
-                        # run filters
+                        # run filters if size is reasonable
+                        width, height = sampleImage.size
+                        if x+w > width or y+h > height:
+                            continue
                         sampleImage = filterLogo(sampleImage,x,y,w,h)
-                        sampleImage.save("test.png")
+                        
+                        # store result
+                        if not os.path.exists(os.path.join(repoPath, c)):
+                            os.makedirs(os.path.join(repoPath, c))
+                        sampleImage.save(os.path.join(repoPath, c, "{}_{}_{}_{}_{}_{}.png".format(c,x,y,w,h,i)))
+                        print("> saving: " + os.path.join(repoPath, c, "{}_{}_{}_{}_{}_{}.png".format(c,x,y,w,h,i)))
+
+                        sampleImage.close()
 
 
